6e48397f4ebea13a560e2a4756c54da0ce029607
Stone <xiezf001@qq.com>
2018-04-21 20:09:19 +0800

    loop

---
+++ game/_references.js
@@ -0,0 +1,4 @@
+﻿/// <autosync enabled = "true">
+/// <reference path="js/GameMain.js" />
+/// <reference path="lib/three.min.js" />
+/// <reference path="lib/whs.js" />
--- game/back/GameMain.js
+++ game/back/GameMain.js
@@ -1,54 +1,71 @@
 ﻿const app = new WHS.App([
     new WHS.ElementModule({
-        container:document.getElementById("app")
+        container: document.getElementById('app')
     }),
     new WHS.SceneModule(),
-    new WHS.DefineModule('camera', new WHS.PerspectiveCamera({
-        position: new THREE.Vector3(0, 10, 50)
-    })),
+    new WHS.CameraModule({
+        position: {
+            y: 10,
+            z: 50
+        }
+    }),
     new WHS.RenderingModule({
-        bgColor: 0xEEAD0E,
+        bgColor: 0x162129,
+
         renderer: {
             antialias: true,
             shadowmap: {
-                type:THREE.PCFSoftShadowMap
+                type: THREE.PCFShadowMap
             }
         }
-    }), { shadow: true },
+    }, { shadow: true }),
     new WHS.OrbitControlsModule(),
     new WHS.ResizeModule()
 ]);
 
-const player = new WHS.Sphere({
+// Sphere
+const sphere = new WHS.Sphere({ // Create sphere comonent.
     geometry: {
         radius: 5,
         widthSegments: 32,
         heightSegments: 32
     },
+
     material: new THREE.MeshPhongMaterial({
         color: 0xF2F2F2
     }),
-    position: new THREE.Vector3(0, 15, 0)
-}).addTo(app);
 
+    position: new THREE.Vector3(0, 5, 0)
+});
+
+sphere.addTo(app);
+
+// Plane
 new WHS.Plane({
     geometry: {
         width: 100,
-        height:100
+        height: 100
     },
+
     material: new THREE.MeshPhongMaterial({ color: 0x447F8B }),
-    rotation: {x:-Math.PI/2}
+
+    rotation: {
+        x: -Math.PI / 2
+    }
 }).addTo(app);
 
+// Lights
 new WHS.PointLight({
     light: {
         intensity: 0.5,
-        distance:100
+        distance: 100
     },
+
     shadow: {
-        fov:90
+        fov: 90
     },
-    position:new THREE.Vector3(0,10,10)
+
+    position: new THREE.Vector3(0, 10, 10)
 }).addTo(app);
 
 new WHS.AmbientLight({
@@ -57,17 +74,12 @@ new WHS.AmbientLight({
     }
 }).addTo(app);
 
-const box = new WHS.Box({ // Create sphere comonent.
-    geometry: [9, 9, 9],
-    position:new THREE.Vector3(0,5,0),
-    material: new THREE.MeshPhongMaterial({
-        color: 0xF2F2F2
-    })
+//循环
+const loop = new WHS.Loop(() => {
+
 });
-box.addTo(app);
 
-new WHS.Loop(() => {
-    box.rotation.y += 0.02;
-}).start(app);
-
+app.addLoop(loop);
+// Start the app
 app.start();
+loop.start();
