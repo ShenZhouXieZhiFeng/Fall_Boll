d4b2fff19299794b4e0416143485a05706ac2e75
Stone <827443469@qq.com>
2018-04-21 23:41:05 +0800

    mousemove controller

--- game/js/GameMain.js
+++ game/js/GameMain.js
@@ -1,94 +1,112 @@
-﻿const app = new WHS.App([
-    new WHS.ElementModule({
-        container:document.getElementById("app")
-    }),
-    new WHS.SceneModule(),
-    new WHS.DefineModule('camera', new WHS.PerspectiveCamera({
-        position: new THREE.Vector3(0, 10, 50)
-    })),
-    new WHS.RenderingModule({
-        bgColor: 0xEEAD0E,
-        renderer: {
-            antialias: true,
-            shadowmap: {
-                type:THREE.PCFSoftShadowMap
+﻿var main = ()=>{
+    const app = new WHS.App([
+        new WHS.ElementModule({
+            container: document.getElementById("app")
+        }),
+        new WHS.SceneModule(),
+        new WHS.DefineModule('camera', new WHS.PerspectiveCamera()),
+        new WHS.RenderingModule({
+            bgColor: 0x1E1E1E,
+            renderer: {
+                antialias: true,
+                shadowmap: {
+                    type: THREE.PCFSoftShadowMap
+                }
             }
+        }), { shadow: true },
+        //new WHS.OrbitControlsModule(),
+        new WHS.ResizeModule()
+    ]);
+
+    //create env_lights
+    const env_lights = new WHS.Group();
+    env_lights.addTo(app);
+    new WHS.PointLight({
+        light: {
+            intensity: 0.5,
+            distance: 100
+        },
+        shadow: {
+            fov: 90
+        },
+        position: new THREE.Vector3(0, 10, 10)
+    }).addTo(env_lights);
+
+    new WHS.AmbientLight({
+        light: {
+            intensity: 0.4
         }
-    }), { shadow: true },
-    //new WHS.OrbitControlsModule(),
-    new WHS.ResizeModule()
-]);
+    }).addTo(env_lights);
+    log(env_lights);
 
-//create env_lights
-const env_lights = new WHS.Group();
-env_lights.addTo(app);
-new WHS.PointLight({
-    light: {
-        intensity: 0.5,
-        distance: 100
-    },
-    shadow: {
-        fov: 90
-    },
-    position: new THREE.Vector3(0, 10, 10)
-}).addTo(env_lights);
+    //create environment
+    const env_objs = new WHS.Group();
+    env_objs.addTo(app);
+    //new WHS.Plane({
+    //    geometry: {
+    //        width: 100,
+    //        height: 100
+    //    },
+    //    material: new THREE.MeshPhongMaterial({ color: 0x447F8B }),
+    //    rotation: { x: -Math.PI / 2 }
+    //}).addTo(env_objs);
 
-new WHS.AmbientLight({
-    light: {
-        intensity: 0.4
-    }
-}).addTo(env_lights);
-log(env_lights);
+    const box = new WHS.Box({
+        geometry: [9, 9, 9],
+        position: new THREE.Vector3(0, 10, 0),
+        material: new THREE.MeshPhongMaterial({
+            color: 0xF2F2F2
+        })
+    });
+    box.addTo(env_objs);
+    log(env_objs);
 
-//create environment
-const env_objs = new WHS.Group();
-env_objs.addTo(app);
-new WHS.Plane({
-    geometry: {
-        width: 100,
-        height:100
-    },
-    material: new THREE.MeshPhongMaterial({ color: 0x447F8B }),
-    rotation: {x:-Math.PI/2}
-}).addTo(env_objs);
+    //create player
+    const player = new WHS.Group();
+    var player_attribute = {
+        speed: 1
+    };
+    const camera = app.manager.get("camera");
+    player.addTo(app);
+    player.add(camera);
+    camera.quaternion.set(-0.15, 0, 0, 1);
+    camera.position.set(0, 15, 40);
+    new WHS.Sphere({
+        geometry: {
+            radius: 5,
+            widthSegments: 32,
+            heightSegments: 32
+        },
+        material: new THREE.MeshPhongMaterial({
+            color: 0x3988D6
+        }),
+        position: new THREE.Vector3(0, 0, 0)
+    }).addTo(player);
+    log(player);
 
-const box = new WHS.Box({ 
-    geometry: [9, 9, 9],
-    position:new THREE.Vector3(0,5,0),
-    material: new THREE.MeshPhongMaterial({
-        color: 0xF2F2F2
-    })
-});
-box.addTo(env_objs);
-log(env_objs);
+    let player_controller = function (event)
+    {
+        let moveX = event.movementX;
+        player.position.x += moveX / 10 * player_attribute.speed;
+    };
 
-//create player
-const player = new WHS.Group();
-player.addTo(app);
-new WHS.Sphere({
-    geometry: {
-        radius: 5,
-        widthSegments: 32,
-        heightSegments: 32
-    },
-    material: new THREE.MeshPhongMaterial({
-        color: 0xF2F2F2
-    }),
-    position: new THREE.Vector3(0, 0, 0)
-}).addTo(player);
-log(player);
+    function register()
+    {
+        document.ontouchmove = player_controller;
+        document.onmousedown = () => {
+            document.onmousemove = player_controller;
+        };
+    };
+    register.call();
 
-let player_controller = function () {
+    //game loop
+    const loop = new WHS.Loop(() => {
 
+    });
+    loop.start();
+
+    //begin
+    app.start();
 };
-
-
-//game loop
-//new WHS.Loop(() => {
-//    env_objs.rotation.y += 0.05;
-//    player_controller();
-//}).start(app);
-
-//begin
-app.start();
+main.call();
 
